5d75baeb76f79514ecbab19b58db2c54bdca6902
Blackraven93 <reblackraven@gmail.com>
2022-10-05 07:49:33 +0900

    Style: migrate vanilla css in ticket

--- pages/components/Ticket.js
+++ pages/components/Ticket.js
@@ -1,20 +1,11 @@
+import style from "../../styles/modules/Info.module.css";
+
 const Ticket = () => {
   return (
-    <div className="ticketInfo">
+    <div className={style.ticketInfo}>
       <h3>티켓 오픈</h3>
       <div>10월 12일</div>
       <div>이벤트브라이트</div>
-
-      <style jsx>{`
-        .ticketInfo {
-          line-height: 2em;
-          font-size: 1em;
-        }
-
-        h3 {
-          color: #ff46c0;
-        }
-      `}</style>
     </div>
   );
 };
