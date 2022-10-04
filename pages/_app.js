7ed0de2e24b45202c16a519daf5ead35298ec5e8
Blackraven93 <reblackraven@gmail.com>
2022-10-05 07:00:34 +0900

    Fix: erase module about fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
 import reset from "../styles/global.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 
 const App = ({ Component, pageProps }) => {
   return (
