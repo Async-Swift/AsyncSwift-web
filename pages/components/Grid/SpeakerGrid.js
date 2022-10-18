ca027d4946eb61cdddf6083305e344b2604774b0
Blackraven93 <reblackraven@gmail.com>
2022-10-19 03:14:09 +0900

    Style: add prefers-color-scheme for darkmode & lightmode

--- pages/components/Grid/SpeakerGrid.js
+++ pages/components/Grid/SpeakerGrid.js
@@ -9,13 +9,6 @@ const SpeakerGrid = ({ sessionTitle, speakers }) => {
       <h3>{sessionTitle}</h3>
       <div className={styles.speakerGrid}>
         {speakers.map((speaker) => {
-          // const noPicktureSpeakers = ["백수영", "홍길동", "주협", "김지홍"];
-
-          // const speakerImagePath = noPicktureSpeakers.includes(
-          //   speaker.speakerName
-          // )
-          //   ? "/default.svg"
-          // : `/speaker/${speaker.speakerName}.svg`;
           const speakerImagePath = `/speaker/${speaker.speakerName}.svg`;
           return (
             <div className={styles.speakerProfileContainer}>
