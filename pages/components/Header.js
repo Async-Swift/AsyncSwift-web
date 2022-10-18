0b32124511b919aae67e93ef71d02dcc96efbeeb
song-cool <oscar2978@naver.com>
2022-10-18 12:55:10 +0900

    Feat: 헤더 레이아웃 적용

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
+import SNS from "./SNS";
 
-import LogoFile from "../../public/AsyncLogo.svg";
+import LogoFile from "../../public/logo-horizental.svg";
 
 import style from "../../styles/modules/Header.module.css";
 
 const Header = () => {
   return (
     <header className={style.header}>
-      <Link href="/">
-        <a>
-          <Image
-            src={LogoFile}
-            alt="AsyncSwift Community Logo"
-            width={62}
-            height={60}
-          />
-        </a>
-      </Link>
+      <div>
+        <Link href="/">
+          <a>
+            <Image
+              src={LogoFile}
+              alt="AsyncSwift Community Logo"
+              width={183}
+              height={40}
+            />
+          </a>
+        </Link>
+        <SNS></SNS>
+      </div>
     </header>
   );
 };
