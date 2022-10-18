e13f910ee3103817e4e9ab2292ecf9e50d705558
song-cool <oscar2978@naver.com>
2022-10-18 12:54:40 +0900

    Feat: description 삭제 및 온보딩 추가

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import HeadMeta from "./components/HeadMeta";
 import Header from "./components/Header";
-import Description from "./components/Description";
+import Onboarding from "./Onboarding/Onboarding";
 import Info from "./components/Info";
 
 import style from "../styles/modules/BackgroundImage.module.css";
@@ -12,7 +12,7 @@ const Home = () => {
 
       <main className={style.backgroundImage}>
         <Header></Header>
-        <Description></Description>
+        <Onboarding></Onboarding>
         <Info></Info>
       </main>
     </>
