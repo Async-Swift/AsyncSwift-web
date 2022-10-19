1137a30c54df4e3fd99544acc3ef2f4bcd90fe5e
Blackraven93 <reblackraven@gmail.com>
2022-10-19 13:16:21 +0900

    Fix: fix undefined case in map

--- pages/components/Grid/SpeakerGrid.js
+++ pages/components/Grid/SpeakerGrid.js
@@ -4,31 +4,33 @@ import styles from "../../../styles/modules/Speaker.module.css";
 import SpeakerProfile from "./SpeakerProfile";
 
 const SpeakerGrid = ({ sessionTitle, speakers }) => {
+  console.log(speakers);
   return (
     <div className={styles.speakerInfoContainer}>
       <h3>{sessionTitle}</h3>
       <div className={styles.speakerGrid}>
-        {speakers.map((speaker, index) => {
-          const speakerImagePath = `/speaker/${speaker.speakerName}.svg`;
-          return (
-            <div key={index} className={styles.speakerProfileContainer}>
-              <div className={styles.speakerProfileImageBox}>
-                <Image
-                  src={speakerImagePath}
-                  alt="AsyncSwift Community Logo"
-                  width={180}
-                  height={180}
-                  priority
-                />
+        {speakers &&
+          speakers.map((speaker, index) => {
+            const speakerImagePath = `/speaker/${speaker.speakerName}.svg`;
+            return (
+              <div key={index} className={styles.speakerProfileContainer}>
+                <div className={styles.speakerProfileImageBox}>
+                  <Image
+                    src={speakerImagePath}
+                    alt="AsyncSwift Community Logo"
+                    width={180}
+                    height={180}
+                    priority
+                  />
+                </div>
+                <SpeakerProfile
+                  key={index}
+                  name={speaker.speakerName}
+                  company={speaker.speakerCompany}
+                ></SpeakerProfile>
               </div>
-              <SpeakerProfile
-                key={index}
-                name={speaker.speakerName}
-                company={speaker.speakerCompany}
-              ></SpeakerProfile>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
