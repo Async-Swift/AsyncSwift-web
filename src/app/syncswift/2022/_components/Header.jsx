0e7cb7f78e6f035c52087f4cc80912a9f744fc26
Blackraven93 <reblackraven@gmail.com>
2023-09-11 23:34:22 +0900

    fix(style): style 마이그레이션을 위한 css 및  js => jsx로 확장자 수정

--- src/app/syncswift/2022/_components/Header.jsx
+++ src/app/syncswift/2022/_components/Header.jsx
@@ -9,8 +9,8 @@ import InfoStyle from "/styles/modules/Info.module.css";
 const Header = () => {
   return (
     <div>
-      <header className={style.header}>
-        <div>
+      <header className="fixed w-screen top-0 py-12 px-4 bg-red opacity-60 backdrop:blur-sm z-10 max-lg:w-[calc(100vw_-_3.2rem)] max-lg:py-8 max-lg:px-7">
+        <div className="flex items-center justify-between">
           <Link href="/">
             <Image
               src={LogoFile}
--- src/app/syncswift/2022/page.jsx
+++ src/app/syncswift/2022/page.jsx
@@ -1,11 +1,35 @@
+import Link from "next/link";
 import Header from "./_components/Header";
 import Main from "./_components/Main/Main";
 import Onboarding from "./_components/Onboarding/Onboarding";
+import Image from "next/image";
+import SNS from "./_components/SNS";
+import Ticket from "./_components/Ticket";
 
 export default function Page({}) {
   return (
     <main>
-      <Header></Header>
+      <div>
+        <header className="fixed w-screen top-0 py-12 px-4 bg-red opacity-60 backdrop:blur-sm z-10 max-lg:w-[calc(100vw_-_3.2rem)] max-lg:py-8 max-lg:px-7">
+          <div className="flex items-center justify-between">
+            <Link href="/">
+              <Image
+                src={LogoFile}
+                alt="AsyncSwift Community Logo"
+                width={183}
+                height={40}
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+              />
+            </Link>
+
+            <SNS locationClassName={InfoStyle.getInfo}></SNS>
+          </div>
+        </header>
+        <Ticket></Ticket>
+      </div>
       <Onboarding></Onboarding>
       <Main></Main>
     </main>
