5c532fad6b0428a804d02e4da86746f7d727e144
Blackraven93 <reblackraven@gmail.com>
2023-09-25 22:45:07 +0900

    fix(alt): SEO관련해 검색 키워드에 AsyncSwift Logo라고 뜨는 alt 수정

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import AsyncSwiftLogo from "/public/AsyncLogo.png";
 import TBDDesign from "/public/syncswift-2023-tbd-design.png";
 import Footer from "./_views/_layouts/Footer";
+
 import { CONFERENCE } from "../utils/consts";
 
 export default async function Page({}) {
@@ -18,24 +19,24 @@ export default async function Page({}) {
 
   return (
     <main className="w-full h-screen">
-      <div className=" flex flex-col justify-between w-full h-full">
-        <header className="flex justify-between w-full pt-9 pl-11 pr-16 max-sm:flex-col-reverse max-sm:gap-6 max-sm:pt-12 max-sm:pl-4">
+      <div className="flex flex-col justify-between w-full h-full ">
+        <header className="flex justify-between w-full pr-16 pt-9 pl-11 max-sm:flex-col-reverse max-sm:gap-6 max-sm:pt-12 max-sm:pl-4">
           <div className="text-3xl font-bold">
             <div>{title}</div>
             <div>{CONFERENCE.TEXT}</div>
           </div>
 
-          <Image alt="AsyncSwift Logo" src={AsyncSwiftLogo} width={62} />
+          <Image alt="AsyncSwift" src={AsyncSwiftLogo} width={62} />
         </header>
         <div className="flex flex-col items-center gap-32">
           <div className="w-10/12">
             <Image alt="SyncSwift 2023 TBD Design" src={TBDDesign}></Image>
           </div>
           <div className="mt">
-            <div className="text-center text-white text-3xl font-extralight">
+            <div className="text-3xl text-center text-white font-extralight">
               {subtitle}
             </div>
-            <div className="text-center text-white text-opacity-50 text-base font-light leading-relaxed mt-2">
+            <div className="mt-2 text-base font-light leading-relaxed text-center text-white text-opacity-50">
               {new Intl.DateTimeFormat("ko-KR").format(new Date(date))}{" "}
               {location}
             </div>
--- src/app/syncswift/2022/page.jsx
+++ src/app/syncswift/2022/page.jsx
@@ -1,31 +1,33 @@
 import Link from "next/link";
-import Header from "./_components/Header";
 import Main from "./_components/Main/Main";
 import Onboarding from "./_components/Onboarding/Onboarding";
 import Image from "next/image";
 import SNS from "./_components/SNS";
 import Ticket from "./_components/Ticket";
+import LogoFile from "/public/LogoHorizental.png";
 
 export default function Page({}) {
   return (
-    <main>
-      <div>
-        <header className="fixed w-screen top-0 py-12 px-4 bg-red opacity-60 backdrop:blur-sm z-10 max-lg:w-[calc(100vw_-_3.2rem)] max-lg:py-8 max-lg:px-7">
-          <div className="flex items-center justify-between">
-            <Link href="/">
-              <Image
-                src={LogoFile}
-                alt="AsyncSwift Community Logo"
-                width={183}
-                height={40}
-                style={{
-                  maxWidth: "100%",
-                  height: "auto",
-                }}
-              />
-            </Link>
+    <main className="w-screen h-full">
+      <div className="relative w-full h-full">
+        <header className="fixed w-screen top-0 py-[20px] px-[40px] bg-[rgba(0,0,0,0.6)] backdrop-blur-[5px]  max-lg:py-[20px] max-lg:px-[16px]">
+          <div className="flex items-center justify-between  w-full h-full ">
+            <div className="w-full h-full">
+              <Link href="/">
+                <Image
+                  src={LogoFile}
+                  alt="AsyncSwift Community Logo"
+                  width={183}
+                  height={40}
+                  style={{
+                    maxWidth: "100%",
+                    height: "auto",
+                  }}
+                />
+              </Link>
+            </div>
 
-            <SNS locationClassName={InfoStyle.getInfo}></SNS>
+            <SNS></SNS>
           </div>
         </header>
         <Ticket></Ticket>
