0e3a22d38da3a3496f0f02cea948831a151dd01c
Blackraven93 <reblackraven@gmail.com>
2023-10-02 00:52:40 +0900

    fix(build): build 실패로 인한 try catch 작성

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,23 +6,26 @@ import Header from "./_views/_layouts/Header";
 import Main from "./_views/_layouts/Main";
 import Footer from "./_views/_layouts/Footer";
 
-export const dynamic = "force-dynamic";
-
 export default async function Page({}) {
-  const supabase = createServerComponentClient({ cookies });
+  try {
+    const supabase = createServerComponentClient({ cookies });
 
-  let {
-    data: [{ title, subtitle, date, location }, ...arg],
-    error,
-  } = await supabase.from("conference").select("*");
+    let {
+      data: [{ title, subtitle, date, location }, ...arg],
+      error,
+    } = await supabase.from("conference").select("*");
 
-  return (
-    <main className="w-full h-screen">
-      <div className="flex flex-col justify-between w-full h-full ">
-        <Header title={title} />
-        <Main subtitle={subtitle} location={location} date={date} />
-        <Footer />
-      </div>
-    </main>
-  );
+    return (
+      <main className="w-full h-screen">
+        <div className="flex flex-col justify-between w-full h-full ">
+          <Header title={title} />
+          <Main subtitle={subtitle} location={location} date={date} />
+          <Footer />
+        </div>
+      </main>
+    );
+  } catch (error) {
+    console.error(error);
+    return <p>{error}</p>;
+  }
 }
