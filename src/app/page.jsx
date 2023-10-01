1ecdbbe71c6cc4d6ece0274644d55b5f887a2738
Blackraven93 <reblackraven@gmail.com>
2023-10-02 01:50:25 +0900

    fix(error): DynamicServerError 해결을 위한 cache 작업

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,17 @@
 import { cookies } from "next/headers";
-
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-
+import { cache } from "react";
 import Header from "./_views/_layouts/Header";
 import Main from "./_views/_layouts/Main";
 import Footer from "./_views/_layouts/Footer";
 
+export const createServerSupabaseClient = cache(() => {
+  const cookieStore = cookies();
+  return createServerComponentClient({ cookies: () => cookieStore });
+});
+
 export default async function Page({}) {
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = await createServerSupabaseClient();
 
   let {
     data: [{ title, subtitle, date, location }, ...arg],
