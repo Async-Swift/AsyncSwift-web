aaee36deff5a6ea2ec21a82764901a1689e72303
Blackraven93 <reblackraven@gmail.com>
2023-10-02 01:18:11 +0900

    test(error): supabase error 패칭 삭제

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,25 @@
 import { cookies } from "next/headers";
 
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import Header from "./_views/_layouts/Header";
 import Main from "./_views/_layouts/Main";
 import Footer from "./_views/_layouts/Footer";
 
 export default async function Page({}) {
-  const supabase = createServerComponentClient({ cookies });
+  // const supabase = createServerComponentClient({ cookies });
 
-  let {
-    data: [{ title, subtitle, date, location }, ...arg],
-    error,
-  } = await supabase.from("conference").select("*");
+  // let {
+  //   data: [{ title, subtitle, date, location }, ...arg],
+  //   error,
+  // } = await supabase.from("conference").select("*");
 
-  if (error) return <p>Error</p>;
+  // if (error) return <p>Error</p>;
+
+  const title = "title";
+  const subtitle = "subtitle";
+  const location = "location";
+  const date = new Date();
 
   return (
     <main className="w-full h-screen">
