65f4d376ed5c12fd8a06db20e1693631057fe875
Blackraven93 <reblackraven@gmail.com>
2023-10-02 02:12:24 +0900

    fix(error): DynamicServerError 해결을 위한 cache 작업

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cache } from "react";
+
 import Header from "./_views/_layouts/Header";
 import Main from "./_views/_layouts/Main";
 import Footer from "./_views/_layouts/Footer";
@@ -11,22 +12,26 @@ export const createServerSupabaseClient = cache(() => {
 });
 
 export default async function Page({}) {
-  const supabase = await createServerSupabaseClient();
+  const supabase = createServerSupabaseClient();
+  try {
+    let {
+      data: [{ title, subtitle, date, location }, ...arg],
+      error,
+    } = await supabase.from("conference").select("*");
 
-  let {
-    data: [{ title, subtitle, date, location }, ...arg],
-    error,
-  } = await supabase.from("conference").select("*");
+    if (error) return <p>Error</p>;
 
-  if (error) return <p>Error</p>;
-
-  return (
-    <main className="w-full h-screen">
-      <div className="flex flex-col justify-between w-full h-full ">
-        <Header title={title} />
-        <Main subtitle={subtitle} location={location} date={date} />
-        <Footer />
-      </div>
-    </main>
-  );
+    return (
+      <main className="w-full h-screen">
+        <div className="flex flex-col justify-between w-full h-full ">
+          <Header title={title} />
+          <Main subtitle={subtitle} location={location} date={date} />
+          <Footer />
+        </div>
+      </main>
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
 }
