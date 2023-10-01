7352df0c81d07871c285a672cd01d37a1628146a
Blackraven93 <reblackraven@gmail.com>
2023-10-01 23:31:40 +0900

    style(font): 영문 font 수정

--- src/app/_views/_layouts/Header.jsx
+++ src/app/_views/_layouts/Header.jsx
@@ -6,8 +6,8 @@ export default function Header({ title }) {
   return (
     <header className="flex justify-between w-full pr-16 pt-9 pl-11 max-sm:gap-6 max-sm:pt-12 max-sm:pl-4">
       <div className="text-3xl font-bold">
-        <div>{title}</div>
-        <div>{CONFERENCE.TEXT}</div>
+        <div className="font-['Montserrat']">{title}</div>
+        <div className="font-['Montserrat']">{CONFERENCE.TEXT}</div>
       </div>
 
       <Image alt="AsyncSwift" src={AsyncSwiftLogo} width={62} />
--- src/app/_views/_layouts/Main.jsx
+++ src/app/_views/_layouts/Main.jsx
@@ -10,7 +10,7 @@ export default function Main({ subtitle, location, date }) {
           <Image alt="SyncSwift 2023 TBD Design" src={TBDDesign}></Image>
         </div>
         <div className="mt">
-          <div className="text-3xl text-center text-white font-extralight">
+          <div className="text-3xl text-center text-white font-extralight font-['Montserrat']">
             {subtitle}
           </div>
           <div className="mt-2 text-base font-light leading-relaxed text-center text-white text-opacity-50">
@@ -20,21 +20,23 @@ export default function Main({ subtitle, location, date }) {
       </div>
       <div className="w-full px-12 py-32">
         <div className="border-[#D9D9D9] border-opacity-50 border border-solid w-full h-full p-10 rounded-lg">
-          <div className="text-white text-opacity-80 text-3xl font-bold font-['Montserrat']">
+          <div className="text-3xl font-bold text-white text-opacity-80 font-['Montserrat']">
             SyncSwift 2023
           </div>
           <ul className="mt-4 font-light leading-6 list-disc opacity-40">
-            <li className="ml-8">행사 일시 : 2023년 10월 21일 13:00 ~ 21:00</li>
+            <li className="ml-8 ">
+              행사 일시 : 2023년 10월 21일 13:00 ~ 21:00
+            </li>
             <li className="ml-8">
               행사 장소 : 체인지업그라운드 2층, 경상북도 포항
             </li>
             <li className="ml-8">행사 문의 : asyncswift@gmail.com</li>
           </ul>
-          <div className="flex flex-col w-full gap-3 mt-10">
-            <button className="bg-[#D9D9D9] bg-opacity-5 border-[#D9D9D9] border-opacity-50 border border-solid h-16 rounded-lg hover:bg-[#D9D9D9] hover:bg-opacity-40">
+          <div className="flex flex-col w-full gap-3 mt-10 text-lg">
+            <button className="font-['Montserrat'] bg-[#D9D9D9] bg-opacity-5 border-[#D9D9D9] border-opacity-50 border border-solid h-16 rounded-lg hover:bg-[#D9D9D9] hover:bg-opacity-40">
               get General Ticket
             </button>
-            <button className="bg-[#D9D9D9] bg-opacity-5 border-[#D9D9D9] border-opacity-50 border border-solid h-16 rounded-lg hover:bg-[#D9D9D9] hover:bg-opacity-40">
+            <button className="font-['Montserrat'] bg-[#D9D9D9] bg-opacity-5 border-[#D9D9D9] border-opacity-50 border border-solid h-16 rounded-lg hover:bg-[#D9D9D9] hover:bg-opacity-40">
               get Scholarship Ticket
             </button>
           </div>
